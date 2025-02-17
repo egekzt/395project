60fefb3b7c59a15ffb0187c11861641b93163871
egekiziltan <egekiziltan2@hotmail.com>
2025-02-17 12:18:32 +0300

    update1
    documentation changes

--- project.py
+++ project.py
@@ -11,11 +11,13 @@ from pyomo.environ import Param,Var,NonNegativeIntegers,Constraint,Set
 
 model = pyomo.ConcreteModel()
 
-pallet_data = pd.read_csv() #file names needs to be implemented after datasets are given out
+#file names needs to be implemented after datasets are given out
+pallet_data = pd.read_csv() 
 vehicle_data = pd.read_csv()
 order_data = pd.read_csv()
 
-vehicle_type_mapping={ #mapper for vehicles
+#mapper for vehicles
+vehicle_type_mapping={ 
     'Truck' : 1,
     'Lorry' : 2,
     'Van' : 3
@@ -30,9 +32,10 @@ vehicle_capacity ={
         (3,1): 6,
         (3,2) : 8,
 }
-
-model.released_product = Var(model.PRODUCTS,model.PALLET_SIZES, model.DAYS, domain=NonNegativeIntegers)  #to be able to update available product we need to be able to tell how much releases when
-model.available_product = Var(model.PRODUCTS,model.PALLET_SIZES, model.DAYS, domain=NonNegativeIntegers) #for available product to make decisions to send & to make sure at the end of the day
+#to be able to update available product we need to be able to tell how much releases when
+model.released_product = Var(model.PRODUCTS,model.PALLET_SIZES, model.DAYS, domain=NonNegativeIntegers)  
+#for available product to make decisions to send & to make sure at the end of the day
+model.available_product = Var(model.PRODUCTS,model.PALLET_SIZES, model.DAYS, domain=NonNegativeIntegers) 
                
 model.ORDER_PRODUCT_PAIRS = Set(initialize={(row['Order ID'], row['Product Type']) 
                                             for _, row in order_data.iterrows()})
@@ -67,13 +70,17 @@ model.PALLET_SIZES = Set(initialize = [1,2])
 model.shipped_product = Var(model.PRODUCTS,model.PALLET_SIZE,model.DAYS,domain=NonNegativeIntegers) 
 #as named its for getting the capacity for given vehicle type with given size
 model.capacity = Param(model.VEHICLE_TYPES, model.PALLET_SIZES,initialize=vehicle_capacity) 
+#tracking cost of vehicles seperately
 model.vehicle_cost = Param(model.VEHICLE_TYPES,initialize={})
 model.vehicle_cost_rented =Param(model.VEHICLE_TYPES,initialize={})
-model.vehicle_count =Param
-model.operations = Var(model.VEHICLE_TYPES,model.DAYS,domain=NonNegativeIntegers) #to hold operation count for each vehicle and day 
-model.vehicle_assigned = Var(model.DAYS,model.VEHICLE_TYPES,PALLET_SIZES)#the amount of vehicle assigned for the specific day, type and size 
+#count of owned vehicles for our company so we can decide after what point they count as rental
+model.vehicle_count =Param(model.VEHICLE_TYPES,initiaize={})
+#to hold operation count for each vehicle and day 
+model.operations = Var(model.VEHICLE_TYPES,model.DAYS,domain=NonNegativeIntegers) 
+#the amount of vehicle assigned for the specific day, type and size 
+model.vehicle_assigned = Var(model.DAYS,model.VEHICLE_TYPES,PALLET_SIZES)
 
-
+#filling the necessary variables from the table vehicle_data
 for index,row in vehicle_data.iterrows():
     vehicle_type = vehicle_type_mapping[row['Vehicle']]
     model.vehicle_cost[vehicle_type] = row['Fixed cost']
@@ -88,7 +95,8 @@ for index, row in vehicle_data.iterrows():
         model.vehicle_count[vehicle_type] = amount
     else:
         print("Error") #normally it should never turn None technically but to be able to catch an unexpected case i just error handle simply
-    
+
+#grabbing the data from pallet_data table
 for index, row in pallet_data.iterrows(): #here from the table we calculate released product count for each day for every size and product type
     product = row['Product']
     release_day = row['Release day']
@@ -96,6 +104,8 @@ for index, row in pallet_data.iterrows(): #here from the table we calculate rele
     size_type = row['Size type']
     model.released_product[product,size_type,release_day] += amount
 
+#to be able to keep track of the available_product each day we sum the released and then extract the shipped amount from that, this gives us the available amount
+#which we need to be able to tell how much we have that we can able to ship + the Q constraint where for each day we have to make sure avaiable_product doesnt exceeds Q
 for product in model.PRODUCTS:
     for size_type in model.PALLET_SIZES:
         for day in model.DAYS:
@@ -103,21 +113,25 @@ for product in model.PRODUCTS:
                 sum(model.released_product[product, size_type, d] for d in range(1, day+1)) 
                 - sum(model.shipped_product[product, size_type, d] for d in range(1, day+1))
 )
-def operation_limit_rule(model,k,d): #to make sure we dont exceed the limit for 3 operations for each vehicle on a day
+#to make sure we dont exceed the limit for 3 operations for each vehicle on a day
+def operation_limit_rule(model,k,d): 
     return model.operations[k,d] <= 3*model.vehicle_count[k]
 model.operation_limit = Constraint(model.VEHICLE_TYPES,model.DAYS,rule=operation_limit_rule)
 
-def waiting_area_limit_rule(model,d): #to make sure amount of pallets left available does not exceed Q
+#to make sure amount of pallets left available does not exceed Q
+def waiting_area_limit_rule(model,d): 
     return sum(model.available_product[i,d] for i in model.PRODUCTS) <= Q #q will be given
 model.waiting_area = Constraint(model.PRODUCTS,rule=waiting_area_limit_rule)
 
-def operation_lateness_rule(model,o,p):#to make sure no order is completed after the date
+#to make sure no order is completed after the date
+def operation_lateness_rule(model,o,p):
     return sum(model.shipped_product[p,s,d] for s in model.PALLET_SIZES for d in range(1,model.due_date[o,p]+1)) >= sum(model.ordered_product[o,p,d] for d in range(1,model.due_date[o,p]+1))
 model.lateness = Constraint(model.ORDER_PRODUCT_PAIRS,model.DAYS, rule=operation_lateness_rule)  
 
-def operation_vehicle_decision_rule(model):
-    return        
+      
 
+#since im using a formula which i pick the amount of vehicle assigned to keep track for each day, based on type we can just assigned-owned to get the rented value
+#on the obj function both variables needs to be multiplied by the corresponding values to their own so we have to keep them sepearate
  def rented_vehicle_rule(model, d, v, p):
     return model.vehicle_rented[d, v, p] >= model.vehicle_assigned[d, v, p] - model.owned_vehicles[v]
 
